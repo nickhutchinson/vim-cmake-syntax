0f46703c34cc6a4fd0598fc634185e4f17b8f0d2
Patrick Boettcher <p@yai.se>
2023-07-13 14:29:34 +0200

    fix bracket-comment-regex
    fixes #27

--- test/bracket-args-and-comment.cmake
+++ test/bracket-args-and-comment.cmake
@@ -13,13 +13,13 @@ The text does not end on a closing bracket of length 0 like ]].
 It does end in a closing bracket of length 1.
 ]=])
 
-message(FATAL_ERROR [=123[
+message(FATAL_ERROR [==[
 This is the first line in a bracket argument with bracket length 1.
 No \-escape sequences or ${variable} references are evaluated.
 This is always one argument even though it contains a ; character.
 The text does not end on a closing bracket of length 0 like ]].
 It does end in a closing bracket of length 1.
-]=123])
+]==])
 
 [[ # this will make a cmake-error but defines a bracket-arguemnt
 cmake_minimum_required(VERSION 4.0 FATAL_ERROR) # Should be string-colored
@@ -43,7 +43,7 @@ It runs until the close bracket.]]
 
 message("First Argument\n" #[[Bracket Comment]] "Second Argument")
 
-#[=12[
+#[=[
 comment
-]=12]
+]=]
 
